08b15c092b9e0d2a82fde6fc8507eb29625647d1
Manoj <manoj@Manoj-MacBook-Pro.local>
2017-05-13 03:05:29 +0530

    first commit

---
+++ jquery-custom-popover.js
@@ -0,0 +1,20 @@
+
+
+$('.pop').data('content', 'this can be an html data which can be used to diaplay<button>test</button>');
+
+
+$(".pop").popover({ trigger: "manual" , html: true, animation:false})
+    .on("mouseenter", function () {
+        var _this = this;
+        $(this).popover("show");
+        $(".popover").on("mouseleave", function () {
+            $(_this).popover('hide');
+        });
+    }).on("mouseleave", function () {
+        var _this = this;
+        setTimeout(function () {
+            if (!$(".popover:hover").length) {
+                $(_this).popover("hide");
+            }
+        }, 300);
+});
